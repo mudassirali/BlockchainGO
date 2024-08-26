e2ca0ea331e08a600a0d03bd234f3f9d2e2e7e7b
mudassirali-steampunk <128065447+mudassirali-steampunk@users.noreply.github.com>
2024-08-25 22:44:03 -0400

    Adding Getts for Transactions

--- src/utils/transaction.go
+++ src/utils/transaction.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Transaction struct {
 	senderBlockchainAddress    string  `json:"sender_blockchain_address"`
 	recipientBlockchainAddress string  `json:"recipient_blockchain_address"`
@@ -8,4 +13,23 @@ type Transaction struct {
 
 func NewTransaction(sender string, recipient string, value float32) *Transaction {
 	return &Transaction{sender, recipient, value}
+}
+
+func (t *Transaction) GetSenderAddress() string {
+	return t.senderBlockchainAddress
+}
+
+func (t *Transaction) GetRecipientAddress() string {
+	return t.recipientBlockchainAddress
+}
+
+func (t *Transaction) GetFunds() float32 {
+	return t.value
+}
+
+func (t *Transaction) Print() {
+	fmt.Printf("%s\n", strings.Repeat("-", 40))
+	fmt.Printf(" sender_blockchain_address      %s\n", t.senderBlockchainAddress)
+	fmt.Printf(" recipient_blockchain_address   %s\n", t.recipientBlockchainAddress)
+	fmt.Printf(" value                          %.1f\n", t.value)
 }
